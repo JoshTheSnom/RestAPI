17294bc51a5ad359042722ec00ab5bb39cadf57c
Kreggy <filip.makrlik@gmail.com>
2020-06-18 23:44:32 +0200

    <3

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -20,17 +20,19 @@ export class CartComponent implements OnInit {
       this.products.getInfo(i.id).subscribe((data: Product) => {
         this.arr.push(data);
         console.log(this.arr);
-        localStorage.setItem('jsonproduct', JSON.stringify(this.arr));
-        const jsonProduct = localStorage.getItem('jsonproduct');
+        localStorage.setItem('product', JSON.stringify(this.arr));
+        const jsonProduct = localStorage.getItem('product');
         this.arr = JSON.parse(jsonProduct);
+        
       });
     });
-    const jsonProduct2 = localStorage.getItem('jsonproduct');
-    this.arr = JSON.parse(jsonProduct2);
-    const result = this.arr.map(res => res.price).reduce((a,b) => a + b, 0);
-    this.pay = result;
+    const jsonProduct2 = localStorage.getItem('product');
+        this.arr = JSON.parse(jsonProduct2);
+        const result = this.arr.map(res => res.price).reduce((a, b) => a + b, 0);
+        this.pay = result;
   }
 
+  
   getProductID(id: number) {
     this.router.navigate(['/info'], {queryParams: {id}});
   }
